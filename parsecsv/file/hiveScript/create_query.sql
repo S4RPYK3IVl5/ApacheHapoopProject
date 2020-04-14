6858f66e5cc7eb32716ab7b38a3725fae9179642
Andrey Saprykin <asaprykin@griddynamics.com>
2020-04-14 17:20:40 +0400

    add join queries

--- parsecsv/file/hiveScript/create_query.sql
+++ parsecsv/file/hiveScript/create_query.sql
@@ -15,3 +15,33 @@ ALTER TABLE events ADD PARTITION (date='2010-01-04') LOCATION '/flume/events/201
 ALTER TABLE events ADD PARTITION (date='2010-01-05') LOCATION '/flume/events/2010/01/05/';
 ALTER TABLE events ADD PARTITION (date='2010-01-06') LOCATION '/flume/events/2010/01/06/';
 ALTER TABLE events ADD PARTITION (date='2010-01-07') LOCATION '/flume/events/2010/01/07/';
+
+CREATE EXTERNAL TABLE countries_blok
+(network string,
+geoname_id string,
+registered_country_geoname_id string,
+represented_country_geoname_id string,
+is_anonymous_proxy string,
+is_satellite_provider string)
+
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+STORED AS TEXTFILE LOCATION '/flume/data/block';
+
+CREATE EXTERNAL TABLE countries_location
+(geoname_id string,
+locale_code string,
+continent_code string,
+continent_name string,
+country_iso_code string,
+country_name string,
+is_in_european_unionv string)
+
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+STORED AS TEXTFILE LOCATION '/flume/data/location';
+
+CREATE TABLE ip_countries as
+    SELECT
+        cb.network as network,
+        cl.country_name as country
+    FROM countries_blok as cb JOIN countries_location as cl
+    ON (cb.registered_country_geoname_id=cl.geoname_id);
---
+++ parsecsv/file/hiveScript/join_query.sql
@@ -0,0 +1,8 @@
+SELECT
+    ic.country,
+    sum(e.price) as sum_price_per_country
+FROM events as e JOIN ip_countries as ic
+ON e.ip = ic.network
+GROUP BY ic.country
+ORDER BY sum_price_per_country DESC
+LIMIT 10;

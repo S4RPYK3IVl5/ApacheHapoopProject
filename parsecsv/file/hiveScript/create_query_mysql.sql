8698723ea50bc9f33a1d996a1922ec7fced2b991
Andrey Saprykin <asaprykin@griddynamics.com>
2020-04-15 09:29:14 +0400

    add sqoop scripts

--- parsecsv/file/hiveScript/create_query_mysql.sql
+++ parsecsv/file/hiveScript/create_query_mysql.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `asaprykin`.`top_category`(
-    `category` VARCHAR(50),
-    `count` INT
+    `category` VARCHAR(100),
+    `count` BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS `asaprykin`.`top_product`(
-    `category` VARCHAR(50),
-    `name` VARCHAR(50),
-    `count` INT
-)
+    `category` VARCHAR(100),
+    `name` VARCHAR(100),
+    `count` BIGINT
+);
--- parsecsv/file/hiveScript/top_query.sql
+++ parsecsv/file/hiveScript/top_query.sql
@@ -1,11 +1,12 @@
 -- Most frequently purchased category
-SELECT
-    category,
-    count(*) as category_count
-FROM events
-GROUP BY category
-ORDER BY category_count DESC
-LIMIT 10;
+CREATE TABLE top_category as
+    SELECT
+        category as category,
+        count(*) as count
+    FROM events
+    GROUP BY category
+    ORDER BY count DESC
+    LIMIT 10;
 
 -- Most frequently purchased product in each category
 -- SELECT
@@ -23,21 +24,22 @@ LIMIT 10;
 --     HAVING count(*) > product_count) <= 10
 -- ORDER BY category_count DESC;
 
-SELECT
-    a.category,
-    a.name,
-    a.prod_count
-FROM
-    (SELECT
-        t.category,
-        t.name,
-        t.prod_count,
-        rank() over (partition by t.category order by t.prod_count desc) as ranked
+CREATE TABLE top_product as
+    SELECT
+        a.category as category,
+        a.name as name,
+        a.prod_count as count
     FROM
         (SELECT
-            category,
-            name,
-            count(*) as prod_count
-            FROM events
-            GROUP BY category, name) as t) as a
-WHERE a.ranked < 10;
+            t.category,
+            t.name,
+            t.prod_count,
+            rank() over (partition by t.category order by t.prod_count desc) as ranked
+        FROM
+            (SELECT
+                category,
+                name,
+                count(*) as prod_count
+                FROM events
+                GROUP BY category, name) as t) as a
+    WHERE a.ranked < 10;

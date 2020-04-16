065299c459f8adc6f94e962241bfaa75528cb630
Andrey Saprykin <asaprykin@griddynamics.com>
2020-04-16 11:37:09 +0400

    finish 8.5 task with spark

--- spark/build.sbt
+++ spark/build.sbt
@@ -6,6 +6,4 @@ scalaVersion := "2.10.7"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.0"
-libraryDependencies += "com.databricks" %% "spark-csv" % "1.5.0"
-libraryDependencies += "org.apache.commons" % "commons-csv" % "1.2"
-
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "1.6.0" % "provided"
--- spark/src/main/scala/Main.scala
+++ spark/src/main/scala/Main.scala
@@ -1,33 +1,39 @@
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.{Dataset, Encoder, Encoders, SQLContext}
 
 object Main {
   def main(args: Array[String]): Unit = {
 
-//    case class Event(name: String, price: String, date: Long, category: String, ip: String)
-//    implicit val enc: Encoder[Event] = Encoders.javaSerialization
-
     val sc = new SparkContext
-    val sqlContext = new SQLContext(sc)
+    val sqlContext = new HiveContext(sc)
     import sqlContext.implicits._
+    import org.apache.spark.sql.expressions.Window
+    import org.apache.spark.sql.functions
 
     val events_df = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "false")
       .load("/flume/events/2010/01/*")
       .toDF("name", "price", "category", "ip", "date")
-    events_df.show()
+//    events_df.show()
+    events_df.cache()
 
-    val top_product = events_df.groupBy("category")
+    // 1 task
+    val top_category = events_df.groupBy("category")
       .count()
       .select("category", "count")
       .orderBy($"count".desc)
-    top_product.show()
+//    top_product.show(10)
 
-
-
-//    val events_ds : Dataset[Event] = events_df.as[Event]
-//    events_ds.show(162)
+    // 2task
+    val window = Window.partitionBy("category").orderBy($"count".desc)
+    val top_product = events_df.groupBy("category","name")
+      .count()
+      .withColumn("rank", functions.rank().over(window).alias("rank"))
+      .select("category", "name", "count", "rank")
+      .filter("rank <= 10")
+    top_product.show(100)
 
   }
 }
